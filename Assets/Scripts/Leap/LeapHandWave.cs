c6ce0817a564c903288b98bf34bda9a1bac2e549
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2014-12-06 11:55:14 +1100

    Apply relative force on impulse to boat object
    - Also removes, unused code

--- Assets/Scripts/Leap/LeapHandWave.cs
+++ Assets/Scripts/Leap/LeapHandWave.cs
@@ -72,35 +72,13 @@ public class LeapHandWave : MonoBehaviour
             Debug.Log(hand);
         }
 
-        Debug.Log(leftHand.PalmVelocity.Magnitude);
-
-        if (frame.Hands.Count >= 2)
-        {
-            // takes the average vector of the forward vector of the hands, used for the
-            // pitch of the plane.
-            Vector3 avgPalmForward = (frame.Hands[0].Direction.ToUnity() + frame.Hands[1].Direction.ToUnity()) * 0.5f;
-
-            Vector3 handDiff = leftHand.PalmPosition.ToUnityScaled() - rightHand.PalmPosition.ToUnityScaled();
-
-            Vector3 newRot = transform.localRotation.eulerAngles;
-            newRot.z = -handDiff.y * 20.0f;
-
-            // adding the rot.z as a way to use banking (rolling) to turn.
-            newRot.y += handDiff.z * 3.0f - newRot.z * 0.03f * transform.rigidbody.velocity.magnitude;
-            newRot.x = -(avgPalmForward.y - 0.1f) * 100.0f;
-
-            float forceMult = 20.0f;
-
-            // if closed fist, then stop the plane and slowly go backwards.
-            if (frame.Fingers.Count < 3)
-            {
-                forceMult = -3.0f;
-            }
-
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(newRot), 0.1f);
-            transform.rigidbody.velocity = transform.forward * forceMult;
+        if (frame.Hands.Count >= 1 && leftHand.IsValid) {
+            rigidbody.AddRelativeForce(leftHand.PalmVelocity.ToUnityScaled() / 10, ForceMode.Impulse);
         }
-
-    }
+//        if (frame.Hands.Count >= 2)
+//        {
+            // something for two hands
+//        }
+//    }
 
 }
