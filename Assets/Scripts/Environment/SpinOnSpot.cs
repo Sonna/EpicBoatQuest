77d4e8c84df4364de6ba44d7a501a345124bb46f
hba <hba1990@gmail.com>
2014-12-07 10:28:59 +1100

    Created script to rotate an object on the spot

---
+++ Assets/Scripts/Environment/SpinOnSpot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinOnSpot : MonoBehaviour 
+{
+    public float rotateSpeed = 5f;
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.RotateAround(transform.position, Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+}
