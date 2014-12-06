2bef4093ad3aac69fe51b71bcaaa430bde2374ad
hba <hba1990@gmail.com>
2014-12-06 12:01:30 +1100

    Updated buoyancy class to make an object float on water

--- Assets/Scripts/Player/ShipBuoyancy.cs
+++ Assets/Scripts/Player/ShipBuoyancy.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class ShipBuoyancy : MonoBehaviour 
 {
+    public float bounceDampening;
+    public float forceFactor;
+    private float triggerHeight;
     private bool inWater = false;
+    private Vector3 uplift;
+    private Vector3 forceDirection;
+    private float boatBottom;
 
     // Use this for initialization
     void Start () 
     {
+        uplift = new Vector3();
         //
     }
     
@@ -16,7 +23,11 @@ public class ShipBuoyancy : MonoBehaviour
     {
         if(inWater)
         {
+            forceDirection = transform.position + transform.TransformDirection(Vector3.down);
+            forceFactor = (1f - transform.position.y);
 
+            uplift = -Physics.gravity * (forceFactor - rigidbody.velocity.y * bounceDampening);
+            rigidbody.AddForceAtPosition(uplift, forceDirection);
         }
     }
 
