c98eedaa45434efdb9ee003099c96a3466290306
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2014-12-06 14:18:10 +1100

    Remove trailing whitespace

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour 
+public class CameraController : MonoBehaviour
 {
     public enum MOUSEBUTTON
     {
@@ -9,7 +9,7 @@ public class CameraController : MonoBehaviour
         RIGHT_CLICK,
         MIDDLE_CLICK
     }
-    
+
     public GameObject boat;
     public float rotateSpeed = 25f;
 
@@ -21,19 +21,19 @@ public class CameraController : MonoBehaviour
             boat = GameObject.FindGameObjectWithTag("Player");
         }
     }
-    
+
     // Update is called once per frame
     void Update ()
     {
         // Make hand gestures here
-        
+
         if(Input.GetMouseButton((int)MOUSEBUTTON.LEFT_CLICK)
            || Input.GetKey(KeyCode.LeftArrow)
            || Input.GetKey(KeyCode.A))
         {
             rotateLeft();
         }
-        
+
         if(Input.GetMouseButton((int)MOUSEBUTTON.RIGHT_CLICK)
            || Input.GetKey(KeyCode.RightArrow)
            || Input.GetKey(KeyCode.D))
