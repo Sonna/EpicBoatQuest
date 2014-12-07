4f62ef0a0f07a3c630821f4f93630de7674d4cb1
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2014-12-07 14:51:05 +1100

    Create a Load Logos script for loading logos for MainMenu

---
+++ Assets/Scripts/Menu/LoadLogo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLogo : MonoBehaviour {
+
+    public Texture2D[] logos;
+    private Texture2D logo;
+
+    // Use this for initialization
+    public void Start()
+    {
+        logo = logos[Random.Range(0, logos.Length)];
+
+        // Get half the screen and desired GUI item width
+        float ScreenX = (float)((UnityEngine.Screen.width * 0.5) - (Width * 0.5));
+        float ScreenY = (float)((UnityEngine.Screen.height) - (Height * 0.5));
+    }
+}
