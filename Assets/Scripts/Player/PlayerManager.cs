d381df1166927dfc146d1dfb3d5fd5e3e461130e
hba <hba1990@gmail.com>
2014-12-07 11:08:04 +1100

    Created Player Manager class for player to set global variables

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        GameManager.Instance.player = this.gameObject;
+        GameManager.Instance.shipStartPosition = transform.position;
+    }
+}
