5b893532e0683c500d73f88e3fca5b3eb818033f
hba <hba1990@gmail.com>
2014-12-07 18:08:59 +1100

    Added boat turning to mouse controls

--- Assets/Scripts/Leap/LeapHandWave.cs
+++ Assets/Scripts/Leap/LeapHandWave.cs
@@ -137,6 +137,16 @@ public class LeapHandWave : MonoBehaviour
             tempCamera.y = transform.position.y;
             Vector3 wind = tempCamera * mousePosDiff.magnitude / windMouseDrag;
             magnitude = mousePosDiff.magnitude;
+            Vector3 localForward = transform.worldToLocalMatrix.MultiplyVector(tempCamera);
+            localForward.y = transform.position.y;
+
+            if(magnitude >= 5.0f)
+            {
+                Vector3 targetDir = localForward * magnitude;
+                float step = 0.25f * Time.deltaTime;
+                Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+                transform.rotation = Quaternion.LookRotation(newDir);
+            }
 
             rigidbody.AddRelativeForce(wind, ForceMode.Impulse);
 
