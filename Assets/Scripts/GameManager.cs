1cced748b82e66b4b4d617343bbc3135a89e4c89
hba <hba1990@gmail.com>
2014-12-07 15:49:12 +1100

    Game Manager handles code to navigate through levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class GameManager : Singleton<GameManager>
 {
+    public float endLevelTimer = 12f;
     public GameObject player;
     public Vector3 shipStartPosition;
     public Texture emptyStarTexture;
     public Texture starTexture;
     public int iconWidth;
     public int iconHeight;
+    public bool gameEnd = false;
     
     public int starsCollected = 0;
     private int totalStars = 0;
@@ -30,6 +32,32 @@ public class GameManager : Singleton<GameManager>
         for(int i = 0; i < totalStars; i++)
         {
             GUI.DrawTexture(new Rect(i * iconWidth, 0, iconWidth, iconHeight), (i < starsCollected ? starTexture : emptyStarTexture));
+        }
+    }
+
+    public void EndLevel(string nextLevel)
+    {
+        gameEnd = true;
+        StartCoroutine(RotateCamera());
+        StartCoroutine(LoadNextLevel(nextLevel));
+
+    }
+
+    private IEnumerator LoadNextLevel(string nextLevel)
+    {
+        yield return new WaitForSeconds(endLevelTimer);
+        Application.LoadLevel(nextLevel);
+    }
+
+    private IEnumerator RotateCamera()
+    {
+        CameraController cameraCont = Camera.main.GetComponent<CameraController>();
+        Camera.main.GetComponent<SmoothFollow>().enabled = false;
+        while(true)
+        {
+            cameraCont.zoomOut();
+            cameraCont.rotateRight();
+            yield return null;
         }
     }
 }
