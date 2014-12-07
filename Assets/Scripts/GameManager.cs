2415cc7937bc65c5d54bf97e7d2cf23a6f892f09
hba <hba1990@gmail.com>
2014-12-07 11:06:21 +1100

    Created a GameManager class to hold global objects and is Singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject player;
+    public Vector3 shipStartPosition;
+    
+    public int starsCollected = 0;
+}
