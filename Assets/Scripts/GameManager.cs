ccd3500ae0336fe90ba214905469329eaba0a8bb
hba <hba1990@gmail.com>
2014-12-07 14:04:42 +1100

    Manager now shows how many stars the player has picked up

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,23 @@ public class GameManager : Singleton<GameManager>
 {
     public GameObject player;
     public Vector3 shipStartPosition;
+    public Texture starTexture;
+    public int iconWidth;
+    public int iconHeight;
     
     public int starsCollected = 0;
+
+
+    void OnGUI()
+    {
+        if (!starTexture) 
+        {
+            return;
+        }
+
+        for(int i = 0; i < starsCollected; i++)
+        {
+            GUI.DrawTexture(new Rect(i * iconWidth, 0, iconWidth, iconHeight), starTexture);
+        }
+    }
 }
