d188bce5d45babdd27b0f38c53dac89b012f6c48
hba <hba1990@gmail.com>
2014-12-07 11:07:08 +1100

    Created a VortexDestroy class to move the player back to the start if it collides with it

---
+++ Assets/Scripts/Environment/VortexDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VortexDestroy : MonoBehaviour 
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the other object is player, move them back to the start
+        if(other.tag.Equals("Player"))
+        {
+            other.rigidbody.velocity = Vector3.zero;
+            other.transform.position = GameManager.Instance.shipStartPosition;
+        }
+    }
+}
