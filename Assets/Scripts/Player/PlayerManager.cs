106004cb9c6d6dccf651a1f100c9bb00f841903a
hba <hba1990@gmail.com>
2015-01-01 13:22:32 +1100

    Added resetting and escape to main menu to the game

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,12 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour 
+{
+    private int counter = 0;
 
     // Use this for initialization
     void Start ()
     {
         GameManager.Instance.player = this.gameObject;
         GameManager.Instance.shipStartPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Reset player location on R press
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(counter >= 3)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+            else
+            {
+                GameManager.Instance.player.transform.position = GameManager.Instance.shipStartPosition;
+            }
+
+            counter++;
+        }
+
+        // Reset player location on R press
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.LoadLevel("MainMenu");
+        }
     }
 }
